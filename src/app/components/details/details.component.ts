ff7fc24748eb004377b254b2e26a1d1a4961505c
Bastien Dumont <bastienxs@gmail.com>
2022-02-04 15:06:15 +0100

    ondestroy details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from 'src/app/models';
@@ -9,36 +9,44 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   gameRating = 0;
-  gameId :string
-  game : Game;
-  routeSub: Subscription
-  gameSub : Subscription
+  gameId: string;
+  game: Game;
+  routeSub: Subscription;
+  gameSub: Subscription;
 
   constructor(
-    private ActivatedRoute : ActivatedRoute,
-    private httpService : HttpService
-  ) {
-    
-  }
+    private ActivatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
 
   ngOnInit(): void {
-    this.routeSub = this.ActivatedRoute.params.subscribe((params : Params) => {
-      this.gameId = params['id']
-      this.getGameDetails(this.gameId)
-    })
+    this.routeSub = this.ActivatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 
   getGameDetails(id: string): void {
     this.gameSub = this.httpService
-    .getGameDetails(id)
-    .subscribe((gameResp : Game) => {
-      this.game = gameResp
-      setTimeout(() => {
-        this.gameRating = this.game.metacritic;
-      })
-    })
+      .getGameDetails(id)
+      .subscribe((gameResp: Game) => {
+        this.game = gameResp;
+        setTimeout(() => {
+          this.gameRating = this.game.metacritic;
+        });
+      });
   }
 
   getColor(value: number): string {
